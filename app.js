71297fd9b3f28a799aa2258fe46a361fbaa75628
RadhikaRJ <radhika7002@gmail.com>
2020-12-25 22:10:13 +0530

    feat: pallindrome check & combinations

--- app.js
+++ app.js
@@ -10,12 +10,39 @@ function clickHandler(){
 
     var[year, month, day] = inputdate.toString().split('-');
 
+    
     if(year===''||month===''||day==='')
     {
         alert("Invalid or Empty input");
     }
     else
     {
+
+
         outputref.innerText = inputdate;
+        dateCombinations(year,month,day);
     }
+}
+
+function checkPallindrome(date,dateReveresed){
+    if(date===dateReveresed){
+        return true;
+    }
+    else return false;
+}
+
+function reversedDate(date){
+    date=date.split('');
+    date=date.reverse();
+    date=date.join('');
+    return date;
+
+}
+
+function CheckPallindromeInCombinations(y,m,d){
+
+    var datepatterns =[(y+m+d),(y+d+m),(m+d+y),(m+y+d),(d+m+y),(d+y+m)];
+    //console.log(datepatterns);
+    
+
 }
