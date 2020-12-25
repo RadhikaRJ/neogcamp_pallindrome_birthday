170fec563f1349426f8a39ea5e6d1d664394da9f
RadhikaRJ <radhika7002@gmail.com>
2020-12-25 23:11:18 +0530

    feat: pallindrome combinations

--- app.js
+++ app.js
@@ -1,48 +1,72 @@
-
 var btnref = document.querySelector("#btn");
 var outputref = document.querySelector("#output");
 
-btnref.addEventListener("click",clickHandler);
+btnref.addEventListener("click", clickHandler);
 
-function clickHandler(){
 
-    var inputdate=document.getElementById('input').value;
+function clickHandler() {
 
-    var[year, month, day] = inputdate.toString().split('-');
+    var inputdate = document.getElementById('input').value;
 
-    
-    if(year===''||month===''||day==='')
-    {
+    var [year, month, day] = inputdate.toString().split('-');
+
+
+    if (year === '' || month === '' || day === '') {
         alert("Invalid or Empty input");
-    }
-    else
-    {
+    } else {
+        var datepatterns = [(year + month + day), (year + day + month), (month + day + year), (month + year + day), (day + month + year), (day + year + month)];
+        var nameOfPatterns = ["yyyy-mm-dd", "yyyy-dd-mm", "mm-dd-yyyy", "mm-yyyy-dd", "dd-mm-yyyy", "dd-yyyy-mm"];
+        var displayPatterns = 
+        [(year+"-"+month+"-"+day),
+        (year+"-"+day+"-"+month),
+        (month+"-"+day+"-"+year),
+        (month+"-"+year+"-"+day),
+        (day+"-"+month+"-"+year),
+        (day+"-"+year+"-"+month)];
+        var flag = 0;
+        for (var i = 0; i < datepatterns.length; i++) {
+            var reversed_date = reversedDate(datepatterns[i]);
+            var display_reverseDate=reversedDate(displayPatterns[i]);
+            var pallindromeResult = checkPallindrome(datepatterns[i], reversed_date);
+            if (pallindromeResult == true && flag == 0) {
+
+                outputref.innerText = displayPatterns[i]+ "=" + display_reverseDate + " Yay!\n\nUsing the combination: " + nameOfPatterns[i] + " your birthdate is proclaimed as Pallindrome!\nFeel free to share this with your friends!";
+                flag += 1;
+            } else if (pallindromeResult == true && flag > 0) {
+                var para = document.createElement("p");
+                var data = "Next combination for which your birthdate is Pallindrome: " + nameOfPatterns[i] + "\n"+displayPatterns[i]+ "=" + display_reverseDate;
+                para.innerText = data;
+                outputref.appendChild(para);
+            }
+        }
+        if(flag==0){
+            
+            outputref.innerText = "Oops! Your birthdate is not a pallindrome number!\nNone of the combinations of your birthdate is pallindrome!"
+        }
 
 
-        outputref.innerText = inputdate;
-        dateCombinations(year,month,day);
+
+
+
     }
+
+
+
+
+
 }
 
-function checkPallindrome(date,dateReveresed){
-    if(date===dateReveresed){
+
+function checkPallindrome(date, dateReveresed) {
+    if (date === dateReveresed) {
         return true;
-    }
-    else return false;
+    } else return false;
 }
 
-function reversedDate(date){
-    date=date.split('');
-    date=date.reverse();
-    date=date.join('');
+function reversedDate(date) {
+    date = date.split('');
+    date = date.reverse();
+    date = date.join('');
     return date;
-
-}
-
-function CheckPallindromeInCombinations(y,m,d){
-
-    var datepatterns =[(y+m+d),(y+d+m),(m+d+y),(m+y+d),(d+m+y),(d+y+m)];
-    //console.log(datepatterns);
-    
 
 }
